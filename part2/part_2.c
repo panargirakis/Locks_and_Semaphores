1192d2e2a0f210caa693acebb4ca456e4f7c0764
Amanda <aqchan@wpi.edu>
2019-02-13 17:54:22 -0500

    created textfile and set up part 2

---
+++ part2/part_2.c
@@ -0,0 +1,20 @@
+/*
+* Project 3 - Part 2 - Taming Massachusetts Drivers
+* Panos Argyrakis and Amanda Chan
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <math.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <time.h>
+
+
+
+int main() {
+
+return 0;
+}
